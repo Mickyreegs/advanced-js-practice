272d4eb3e89116b4b6021ec182a86c8ee5b6c2fa
Mickyreegs <w58fyus5@students.codeinstitute.net>
2025-02-23 20:19:25 +0000

    Add destructuring JS and challenge

--- assets/script.js
+++ assets/script.js
@@ -233,3 +233,113 @@ let reverseSort = updateSortReverse(moreMixedLetters, 'n', 'm', 'o');
 console.log(reverseSort);
 
 console.log(mixedLetters);
+
+//-----------Destructuring:
+/*
+Destructuring gives us a faster way to take an array of items and turn those items into distinct, individual variables.
+Instead of referencing each element of the  array by its index and declaring each variable individually,
+we can use destructuring  to do it all on a single line of code.
+*/
+
+// Destructuring arrays
+let ages = [30, 26, 27];
+// let john = ages[0];
+// let mary = ages[1];
+// let joe = ages[2];
+let [john, mary, joe] = ages;
+console.log(john, mary, joe);
+
+/*
+In the above, we defined an array of variable
+names, john, mary and joe. I'll set it equal to  ages, and through destructuring assignment,
+ES6 automatically assigns the first value in the  array to the variable john, the second to the
+variable mary, and the third to the variable  joe. Using ES6 destructuring assignment,
+we were able to reduce the three variable  assignments above to just a single line of code.
+*/
+
+// Destructuring objects
+let jobs = {
+    mike: "designer",
+    jill: "developer",
+    alicia: "accountant",
+};
+let {mike, jill, alicia} = jobs;
+console.log(mike, jill, alicia);
+
+// Destructuring subsets
+let languages = ["english", "french", "spanish", "german", "japanese"];
+let [johnNative, johnSecondary] = languages;
+console.log(johnNative, johnSecondary);
+
+let [, , maryNative, marySecondary] = languages;
+console.log(maryNative, marySecondary);
+
+let languages2 = {
+    firstLanguage: 'english',
+    secondLanguage: 'french',
+    thirdLanguage: 'german',
+    fourthLanguage: 'japanese',
+};
+let {firstLanguage, thirdLanguage} = languages2;
+console.log(firstLanguage, thirdLanguage);
+
+
+// Using rest parameter syntax
+let fruits = ['apple',  'orange', 'banana', 'peach', 'cherry'];
+let[favourite, secondFavourite, ...others] = fruits;
+console.log(favourite);
+console.log(secondFavourite);
+console.log(others);
+
+let favouriteFoods = {
+    brian: 'pizza',
+    anna: 'pasta',
+    sarah: 'vegetarian',
+    andrea: 'steak',
+};
+let{brian, anna, ...rest} = favouriteFoods;
+console.log(brian);
+console.log(anna);
+console.log(rest);
+
+
+//-----------Destructuring Challenge:
+let studenti = [
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 80, art: 95},
+    },
+    {
+        name: 'John',
+        subjects: ['art', 'cad', 'english', 'maths', 'science'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 93, maths: 77, art: 95},
+    },
+    {
+        name: 'Fran',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 87, art: 95},
+    }
+];
+
+const makeList = (arr, student) => {
+    for(let itm of arr) {
+        if(itm.name == student) {
+          return itm.subjects;
+        }
+    }
+};
+
+let [first, second, ...remainder] = makeList(studenti, 'John');
+console.log(first, second, remainder);
+
+
