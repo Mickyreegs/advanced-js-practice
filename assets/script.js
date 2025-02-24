061982c39c3677ced1e683c1f6263886ef086bf8
Mickyreegs <w58fyus5@students.codeinstitute.net>
2025-02-24 20:45:22 +0000

    Add filter() method part 1

--- assets/script.js
+++ assets/script.js
@@ -187,7 +187,7 @@ console.log('Sum2:', sumNew);
 
 /*
 In the above, what if you wanted to sum an arbitrary number of parameters?
-JavaScript will allow you to pass in as many  rguments as you want regardless of how many
+JavaScript will allow you to pass in as many  arguments as you want regardless of how many
 are specified when you define the function, so I can simply pass 1, 2, 3, 4, 5, and 6,
 and the last three will just be ignored. If I log  this to the console as sum2, the value is still 6,
 which is obviously not what we want. This is  where the rest parameter syntax comes into play.
@@ -421,6 +421,39 @@ const studentis = [
   console.log(studentsWithAges);
 
 
+//map challenge
+let students1 = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 80, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 93, maths: 77, art: 95},
+    },
+    {
+        name: 'Fran',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 87, art: 95},
+    }
+];
+
+// single line, used different variable names so as not to clash with those below.
+let [John, ...Rest] = students1.map(itm => [itm.name, itm.results]);
+console.log(John);
+console.log(Rest);
+
 
 /*
 filter() iterates over an array of items, and filters the  array to only a specified set of results.
@@ -432,6 +465,71 @@ into the callback function, and if the callback  function returns true when give
 that element will be included in the results.  Otherwise, the element will be filtered out.
 When the filter method is complete, the result  is a new array containing the filtered results.
 */
+
+/*
+Like the map method, the filter method uses a  callback function which we pass to the method,
+and which will be executed on every element of  the array. The callback function must return a
+test that will evaluate to either true or false.  If the array item being tested passes the test
+in the callback function, it will be included  in the results returned by the filter method.
+Otherwise, it'll be filtered out.
+*/
+
+// Simple Filtering
+const people = [
+    {
+      name: 'Michael',
+      age: 23,
+    },
+    {
+      name: 'Lianna',
+      age: 16,
+    },
+    {
+      name: 'Paul',
+      age: 18,
+    },
+  ];
+
+  const oldEnough = people.filter(person => person.age >= 21);
+  console.log(oldEnough);
+
+  const paul = people.filter(p => p.name === 'Paul');
+  console.log(paul);
+  
+  
+  // Complex Filtering
+  const students2 = [
+    {
+      id: 1,
+      name: 'Mark',
+      profession: 'Developer',
+      skills: [
+        { name: 'javascript', yrsExperience: 1 },
+        { name: 'html', yrsExperience: 5 },
+        { name: 'css', yrsExperience: 3 },
+      ]
+    },
+    {
+      id: 2,
+      name: 'Ariel',
+      profession: 'Developer',
+      skills: [
+        { name: 'javascript', yrsExperience: 0 },
+        { name: 'html', yrsExperience: 4 },
+        { name: 'css', yrsExperience: 2 },
+      ]
+    },
+    {
+      id: 3,
+      name: 'Jason',
+      profession: 'Designer',
+      skills: [
+        { name: 'javascript', yrsExperience: 1 },
+        { name: 'html', yrsExperience: 1 },
+        { name: 'css', yrsExperience: 5 },
+      ]
+    },
+  ];
 
 
 
